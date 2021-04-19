628746093383cc0fbe38e4d986685ce9ef1988d5
wu-qing-157 <wuqing157@gmail.com>
2021-04-19 14:43:06 +0900

    Initialize Lab 3

---
+++ acmdb-lab3/src/java/simpledb/RecordId.java
@@ -0,0 +1,65 @@
+package simpledb;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+/**
+ * A RecordId is a reference to a specific tuple on a specific page of a
+ * specific table.
+ */
+public class RecordId implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    private final PageId pid;
+    private final int tupleno;
+
+    /**
+     * Creates a new RecordId referring to the specified PageId and tuple
+     * number.
+     *
+     * @param pid     the pageid of the page on which the tuple resides
+     * @param tupleno the tuple number within the page.
+     */
+    public RecordId(PageId pid, int tupleno) {
+        this.pid = pid;
+        this.tupleno = tupleno;
+    }
+
+    /**
+     * @return the tuple number this RecordId references.
+     */
+    public int tupleno() {
+        return tupleno;
+    }
+
+    /**
+     * @return the page id this RecordId references.
+     */
+    public PageId getPageId() {
+        return pid;
+    }
+
+    /**
+     * Two RecordId objects are considered equal if they represent the same
+     * tuple.
+     *
+     * @return True if this and o represent the same tuple
+     */
+    @Override
+    public boolean equals(Object o) {
+        return o instanceof RecordId && tupleno == ((RecordId) o).tupleno && Objects.equals(pid, ((RecordId) o).pid);
+    }
+
+    /**
+     * You should implement the hashCode() so that two equal RecordId instances
+     * (with respect to equals()) have the same hashCode().
+     *
+     * @return An int that is the same for equal RecordId objects.
+     */
+    @Override
+    public int hashCode() {
+        return pid.hashCode() * 31 + tupleno;
+    }
+
+}
